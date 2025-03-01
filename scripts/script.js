8ce7c4f1ee89d647a754665d28e2ca4f1b6e77c3
alfa96git <alfa.sunny.96@gmail.com>
2025-03-02 03:15:21 +0600

    added new page with color

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,14 @@ const currentDate = new Date();
 todayDate.innerText = currentDate.toDateString();
 
 
-
+        const cardTitle =document.querySelectorAll('.card-title');
+        const historyLog =document.getElementById('history')
 
 let task = 6;
 let count = 23;
-
 const btns = document.querySelectorAll('#completed')
 for (const btn of btns)
-    btn.addEventListener('click', function (event) {
-        event.preventDefault();
+    btn.addEventListener('click', function () {
         alert('board updated successfully')
 
         const taskKombe = document.getElementById('task').innerText;
@@ -22,11 +21,92 @@ for (const btn of btns)
         if (task > 0) {
             task--;
             count++;
-
+        
             document.getElementById('task').innerText = task;
             document.getElementById('count').innerText = count;
         }
         else {
-
         }
-    })
+        document.getElementById('completed').disable = true;
+        
+
+        const newElement = document.createElement("div");
+
+    newElement.innerHTML = `
+    <div class="bg-slate-100 p-3 mb-7 rounded-xl"> 
+        <p> You have Complete The Task ${
+          cardTitle.innerText 
+        } at ${format12HourTime()} </p>
+        </div>
+    `;
+    function format12HourTime() {
+        const nowTime = new Date();
+        let hours = nowTime.getHours();
+        let minutes = nowTime.getMinutes();
+        let seconds = nowTime.getSeconds();
+        const amPm = hours >= 12 ? "PM" : "AM";
+  
+        hours = hours % 12 || 12;
+  
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+        return `${hours}:${minutes}:${seconds} ${amPm}`;
+      }
+    historyLog.appendChild(newElement);
+}); 
+
+
+
+// const cardTitle =document.querySelectorAll('.card-title');
+//         const historyLog =document.getElementById('history')
+        
+
+//         const newElement = document.createElement("div");
+
+//     newElement.innerHTML = `
+//     <div class="bg-slate-100 p-3 mb-7 rounded-xl"> 
+//         <p> You have Complete The Task ${
+//           cardTitle.btn.innerText
+//         } at ${format12HourTime()} </p>
+//         </div>
+//     `;
+//     function format12HourTime() {
+//         const nowTime = new Date();
+//         let hours = nowTime.getHours();
+//         let minutes = nowTime.getMinutes();
+//         let seconds = nowTime.getSeconds();
+//         const amPm = hours >= 12 ? "PM" : "AM";
+  
+//         hours = hours % 12 || 12;
+  
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+//         return `${hours}:${minutes}:${seconds} ${amPm}`;
+//       }
+//     historyLog.appendChild(newElement);
+
+const colors = ["indigo", "#33FF57", "yellow", "pink", "#3357FF"]; 
+
+    document.getElementById("changeBg").addEventListener("click", function() {
+        const randomIndex = Math.floor(Math.random() * colors.length); 
+        document.body.style.backgroundColor = colors[randomIndex]; 
+    });
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
